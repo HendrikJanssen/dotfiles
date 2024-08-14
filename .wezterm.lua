00bbe3f479b6094f76f6eb9ed9f4a0c9b8065920
Hendrik Janßen <kontakt@hendrik-janssen.de>
2024-08-14 17:19:13 +0200

    Fixed Option key

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,5 +6,8 @@ config.color_scheme = "Neutron"
 config.font = wezterm.font("GeistMono Nerd Font Propo")
 config.font_size = 14
 config.use_fancy_tab_bar = false
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0", "zero=0" }
 
 return config
