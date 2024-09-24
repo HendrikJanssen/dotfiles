e17445911e237915750b3c3fda3a8cdbef7fdeaa
hendrik <hendrik.janssen@newcubator.com>
2024-09-24 10:57:30 +0200

    Removed default wezterm keybinds, added tab management keys

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,6 @@ config.color_scheme = "Neutron"
 config.font = wezterm.font("GeistMono Nerd Font Propo")
 config.font_size = 15
 config.use_fancy_tab_bar = false
---config.send_composed_key_when_left_alt_is_pressed = true
---config.send_composed_key_when_right_alt_is_pressed = true
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0", "zero=0" }
 config.freetype_load_flags = "DEFAULT|NO_AUTOHINT"
 config.window_decorations = "TITLE|RESIZE|MACOS_FORCE_DISABLE_SHADOW"
@@ -18,4 +16,23 @@ config.window_padding = {
 	top = 0,
 	bottom = 0,
 }
+
+config.disable_default_key_bindings = true
+local act = wezterm.action
+config.keys = {}
+for i = 1, 8 do
+	-- CTRL + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+table.insert(config.keys, {
+	key = "t",
+	mods = "CMD",
+	action = act.SpawnTab("CurrentPaneDomain"),
+})
+
 return config
