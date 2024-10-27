af2facd252edc3ad5336a77451024137277defa4
hendrik <hendrik.janssen@newcubator.com>
2024-10-27 14:16:10 +0100

    netrw still loads

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,8 +15,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 
 -- gruvbox settings
 vim.g.gruvbox_contrast_dark = "hard"
